59c2f63057e27af51b612611fee050c470a8e5dd
Wahyu Stiawan <wahyustiawan763@gmail.com>
2025-09-29 22:55:36 +0700

    fix: remove duplicate admin user creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,30 +12,25 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run()
-{
-    // Create admin user
-    User::create([
-        'name' => 'wahyu stiawan',
-        'email' => 'wahyustiawan763@gmail.com',
-        'password' => bcrypt('password'),
-        'role' => User::ROLE_ADMIN
-    ]);
+    {
+        // HAPUS BAGIAN INI - karena sudah ada di AdminUserSeeder
+        // User::create([
+        //     'name' => 'wahyu stiawan',
+        //     'email' => 'wahyustiawan763@gmail.com',
+        //     'password' => bcrypt('password'),
+        //     'role' => User::ROLE_ADMIN
+        // ]);
 
-    // Create sample customers
-    User::factory(10)->create([
-        'role' => User::ROLE_CUSTOMER
-    ]);
-
-
+        // Create sample customers
+        User::factory(10)->create([
+            'role' => User::ROLE_CUSTOMER
+        ]);
 
         $this->call([
             AdminUserSeeder::class,
             TableSeeder::class,
         ]);
 
-
         $this->call(MenuSeeder::class);
     }
-
-    
 }
