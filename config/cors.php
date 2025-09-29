579b00c8f3327cbc7d4ff1f0ac0f6904d431afec
Wahyu Stiawan <wahyustiawan763@gmail.com>
2025-09-30 01:57:55 +0700

    update CORS for frontend domain

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,17 @@
 <?php
 
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Ganti dengan URL frontend kamu
+    'allowed_origins' => [
+        'https://frontend-production-c996.up.railway.app',
+        'https://pitychick-production.up.railway.app',
+        'http://localhost:3000',
+        'http://localhost:5173'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false, // Set false dulu
 ];
